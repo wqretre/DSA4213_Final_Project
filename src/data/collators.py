a7eadbc7fae8439c8ed46fecb19ff1dbf6374ab5
Liu Jianyong <143146633+wqretre@users.noreply.github.com>
2025-11-15 13:21:14 +0800

    Update collators.py

--- src/data/collators.py
+++ src/data/collators.py
@@ -1,5 +1,6 @@
 import torch
 
+
 # pad inputs_id and lebels to the same length
 def collate_fn(batch, pad_id):
     input_ids = [torch.tensor(x["input_ids"]) for x in batch]
@@ -7,6 +8,7 @@ def collate_fn(batch, pad_id):
     input_ids = torch.nn.utils.rnn.pad_sequence(input_ids, batch_first=True, padding_value=pad_id)
     labels = torch.nn.utils.rnn.pad_sequence(labels, batch_first=True, padding_value=-100)
     return {"input_ids": input_ids, "labels": labels}
+
 
 def collate_fn_leftpad(batch, pad_id):
     batch = [torch.tensor(x, dtype=torch.long) for x in batch]
