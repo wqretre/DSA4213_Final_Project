435cbfc718bec0d79ad66f95d1e7479ed7a2e9be
Liu Jianyong <143146633+wqretre@users.noreply.github.com>
2025-11-15 11:39:58 +0800

    Update collators.py

--- src/data/collators.py
+++ src/data/collators.py
@@ -1,17 +1,16 @@
 import torch
 
 # pad inputs_id and lebels to the same length
-def collate_fn(batch):
+def collate_fn(batch, pad_id):
     input_ids = [torch.tensor(x["input_ids"]) for x in batch]
     labels = [torch.tensor(x["labels"]) for x in batch]
-    input_ids = torch.nn.utils.rnn.pad_sequence(input_ids, batch_first=True, padding_value=tokenizer.pad_token_id)
+    input_ids = torch.nn.utils.rnn.pad_sequence(input_ids, batch_first=True, padding_value=pad_id)
     labels = torch.nn.utils.rnn.pad_sequence(labels, batch_first=True, padding_value=-100)
     return {"input_ids": input_ids, "labels": labels}
 
-def collate_fn_leftpad(batch):
+def collate_fn_leftpad(batch, pad_id):
     batch = [torch.tensor(x, dtype=torch.long) for x in batch]
     max_len = max(len(x) for x in batch)
-    pad_id = tokenizer.pad_token_id
     # left padding
     padded = torch.stack([
         torch.cat([torch.full((max_len - len(x),), pad_id, dtype=torch.long), x])
