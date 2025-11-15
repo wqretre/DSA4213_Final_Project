2aaf4646941b5e45d44cbda9d38334f410a09190
Liu Jianyong <143146633+wqretre@users.noreply.github.com>
2025-11-15 13:45:35 +0800

    Update plotting.py

--- src/models/plotting.py
+++ src/models/plotting.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 
+
 def plot_loss_curve(trainer):
-  # plot the loss curve
-  train_losses = [x["loss"] for x in trainer.state.log_history if "loss" in x]
-  
-  plt.figure(figsize=(8,5))
-  plt.plot(train_losses, label="Train Loss")
-  plt.xlabel("Steps")
-  plt.ylabel("Loss")
-  plt.title("Training Loss")
-  plt.legend()
-  plt.show()
+    # plot the loss curve
+    train_losses = [x["loss"] for x in trainer.state.log_history if "loss" in x]
+
+    plt.figure(figsize=(8, 5))
+    plt.plot(train_losses, label="Train Loss")
+    plt.xlabel("Steps")
+    plt.ylabel("Loss")
+    plt.title("Training Loss")
+    plt.legend()
+    plt.show()
